a63e1b6cb4f7402103eef5d266f01851236a826a
Nguyenthai27 <ptejthaii@gmail.com>
2024-01-27 09:01:00 +0700

    commit exam IASF

--- src/main/java/com/example/exam_isaf/Repository/EmployeeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.exam_isaf.Repository;
-
-import com.example.exam_isaf.entity.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public class EmployeeRepository extends JpaRepository<Employee, Long> {
-
-}
